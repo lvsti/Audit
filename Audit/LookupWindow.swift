dea080e758338961975e534110fa29262d8b6dee
Tamas Lustyik <elveestei@gmail.com>
2019-02-16 11:34:55 +0100

    Ported LookupWindow from Cameo

---
+++ Audit/LookupWindow.swift
@@ -0,0 +1,17 @@
+//
+//  LookupWindow.swift
+//  Audit
+//
+//  Created by Tamás Lustyik on 2019. 02. 16..
+//  Copyright © 2019. Tamas Lustyik. All rights reserved.
+//
+
+import Cocoa
+
+class LookupWindow: NSWindow {
+    
+    override var canBecomeKey: Bool {
+        return true
+    }
+    
+}
